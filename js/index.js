c898292c95944b85d76dd80772e4c3bcd30603cd
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-23 19:24:56 +0100

    add marker and infowindow

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'),
     {
-        center: {lat: -34.397, lng: 150.644},
+        center: {lat: 51.5074, lng: 0.1278},
         zoom: 8
     });
+
+    var fulham = {lat:51.4773, lng:0.2017};
+    var marker = new google.maps.Marker({
+        position: fulham,
+        map: map,
+        title: "Fulham Marker"
+    });
+    var infowindow = new google.maps.InfoWindow({
+        content:'info window content'
+    });
+    marker.addListener('click', function(){
+        infowindow.open(map, marker);
+    })
 }
