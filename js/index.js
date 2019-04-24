a7151d98fd0b445bbd45c4176afc44ee6701525d
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-24 02:07:31 +0100

    updated location info

--- js/index.js
+++ js/index.js
@@ -87,22 +87,128 @@ function initMap() {
   //constructor creates a new map -only center and zoom are requested
     map = new google.maps.Map(document.getElementById('map'),
     {
-        center: {lat: 40.7413549, lng: -73.9980244},
-        zoom: 13,
-        styles: mapDarkStyle,
-        mapTypeControl: false
+      center: {lat: 40.7413549, lng: -73.9980244},
+      zoom: 13,
+      styles: mapDarkStyle,
+      mapTypeControl: false
     });
     
 
     //for now i am hardcoding the places, later i will connect to db
     let locations = [
-        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-      ];
+      {
+        id: '4c7cc27e3e90a1cdfb5243be',
+        title: 'Cafe Milano',
+        type: 'Cafe',
+        location: {
+          lat: 51.46099454945425,
+          lng: -0.21752025208051115
+        },
+        streetAddress: '204 Upper Richmond Rd',
+        city: 'London ',
+        zipCode: 'SW15 6TD',
+        mapDirection:
+          "<a href='https://goo.gl/maps/RfogWdvZ89K2'>Get Directions</a>",
+        imageURL: "<a href='https://goo.gl/maps/jHmUDqUb6U92'></a>",
+        extraInfo: ''
+      },
+      {
+        id: '4c78016e97028cfa4e4dd6fe',
+        title: 'Cafe Nunu',
+        type: 'Cafe',
+        location: {
+          lat: 51.46791804300835,
+          lng: -0.20908542453073797
+        },
+        streetAddress: '6A Ranelagh Gardens',
+        city: 'London ',
+        zipCode: 'SW6 3UB',
+        mapDirection:
+          "<a href='https://goo.gl/maps/RdeLb7nT2K22'>Get Directions</a>",
+        imageURL: "<a href='https://goo.gl/maps/qvkq4qBN5tn'></a>",
+        extraInfo: ''
+      },
+      {
+        id: '4b9fe98af964a520d54937e3',
+        title: 'Cafe Nero',
+        type: 'Cafe',
+        location: {
+          lat: 51.459819,
+          lng: -0.211806
+        },
+        streetAddress: '112 Upper Richmond Rd',
+        city: 'London ',
+        zipCode: 'SW15 2SP',
+        mapDirection:
+          "<a href='https://goo.gl/maps/KcLqQqLvxh52'>Get Directions</a>",
+        imageURL: "<a href='https://goo.gl/maps/WkqxF1wFLev'></a>",
+        extraInfo: ''
+      },
+      {
+        id: '559b959f498e191eb0698efa',
+        title: 'Costa Coffee',
+        type: 'Cafe',
+        location:{
+        lat: 51.465003,
+        lng: -0.214407
+        },
+        streetAddress: '26 Putney High St',
+        city: 'London ',
+        zipCode: 'SW15 1SN',
+        mapDirection:
+          "<a href='https://goo.gl/maps/vazMns9n5DP2'>Get Directions</a>",
+        imageURL: "<a href='https://goo.gl/maps/rzBvABMoi4A2'></a>",
+        extraInfo: ''
+      },
+      {
+        id: '4ebf15afd5fb50d70f350c59',
+        title: 'Artisan Coffee',
+        type: 'Cafe',
+        location: {
+          lat: 51.461226,
+          lng: -0.219454
+        },
+        streetAddress: '203 Upper Richmond Rd',
+        city: 'London ',
+        zipCode: 'SW15 6SG',
+        mapDirection:
+          "<a href='https://goo.gl/maps/U44bLMrbN9w'>Get Directions</a>",
+        imageURL: "<a href='https://goo.gl/maps/j2JVhZKQz9n'></a>",
+        extraInfo: ''
+      },
+      {
+        id: '4fd09d28e4b0da17b03a3264',
+        title: 'Cafe Parisienne',
+        type: 'Cafe',
+        location: {
+          lat: 51.461578711507386,
+          lng: -0.22001220017854142
+        },
+        streetAddress: '225 Lavender Hill',
+        city: 'London ',
+        zipCode: 'SW11 1JR',
+        mapDirection:
+          "<a href='https://goo.gl/maps/x3L46FgDAC92'>Get Directions</a>",
+        URL: `https://goo.gl/maps/jHmUDqUb6U92`,
+        extraInfo: ''
+      },
+      {
+        id: '4b546704f964a520f8b927e3',
+        title: 'Ground Coffee Society',
+        type: 'Cafe',
+        location: {
+          lat: 51.467227,
+          lng: -0.219392
+        },
+        streetAddress: '79 Lower Richmond Rd',
+        city: 'London ',
+        zipCode: 'SW15 1ET',
+        mapDirection:
+          "<a href='https://goo.gl/maps/wfSwoMqmZ4F2'>Get Directions</a>",
+        URL: 'https://goo.gl/maps/1RfcDSRoiw82',
+        extraInfo: ''
+      }
+    ]
     
     
     // Style the markers a bit. This will be our listing marker icon.
@@ -118,7 +224,7 @@ function initMap() {
       var title = locations[i].title;
       // Create a marker per location, and put into markers array.
       var marker = new google.maps.Marker({
-        map: map,//shows markers on load
+        
         position: position,
         title: title,
         animation: google.maps.Animation.DROP,
