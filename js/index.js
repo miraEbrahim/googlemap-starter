abf8c0b1d0d80286a32815cb0da7fe96e0c580ea
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-24 01:20:29 +0100

    markers shown when show button is presses

--- js/index.js
+++ js/index.js
@@ -3,24 +3,96 @@ let map;
 let markers = [];
 
 function initMap() {
+  //create a map style array
+  let mapDarkStyle =   [
+    {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+    {
+      featureType: 'administrative.locality',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [{color: '#263c3f'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#6b9a76'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{color: '#38414e'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#212a37'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#9ca5b3'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{color: '#746855'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#1f2835'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#f3d19c'}]
+    },
+    {
+      featureType: 'transit',
+      elementType: 'geometry',
+      stylers: [{color: '#2f3948'}]
+    },
+    {
+      featureType: 'transit.station',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [{color: '#17263c'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#515c6d'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [{color: '#17263c'}]
+    }
+  ];
+  //constructor creates a new map -only center and zoom are requested
     map = new google.maps.Map(document.getElementById('map'),
     {
         center: {lat: 40.7413549, lng: -73.9980244},
-        zoom: 13
+        zoom: 13,
+        styles: mapDarkStyle,
+        mapTypeControl: false
     });
-    //marker for single location 
-    // var fulham = {lat:51.4773, lng:0.2017};
-    // var marker = new google.maps.Marker({
-    //     position: fulham,
-    //     map: map,
-    //     title: "Fulham Marker"
-    // });
-    // var infowindow = new google.maps.InfoWindow({
-    //     content:'info window content'
-    // });
-    // marker.addListener('click', function(){
-    //     infowindow.open(map, marker);
-    // })
+    
 
     //for now i am hardcoding the places, later i will connect to db
     let locations = [
@@ -31,44 +103,72 @@ function initMap() {
         {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
         {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
       ];
+    
+    
+    // Style the markers a bit. This will be our listing marker icon.
+    // var defaultIcon = makeMarkerIcon('0091ff');
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    // var highlightedIcon = makeMarkerIcon('FFFF24');
     let largeInfowindow = new google.maps.InfoWindow();
-    let bounds = new google.maps.LatLngBounds();
-    //the following group uses location array to create an array of markers on initialize
-    for (let i = 0; i < locations.length; i++) {
-        //get the position from the location array
-        let position = locations[i].location;
-        let title = locations[i].title;
-        //create a marker per location and put into markers array
-        var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title:title,
-            animation: google.maps.Animation.DROP,
-            id:i
-    });
-        //push the marker to our array of markers
-        markers.push(marker);
-        //create a onClick event to open an infowindow at each marker
-        marker.addListener('click', function(){
-            populateInfoWindow(this, largeInfowindow);
-        });
-        //Extends the boundries of the map for each marker
-        bounds.extend(marker.position);
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+      // Get the position from the location array.
+      let position = locations[i].location;
+      let title = locations[i].title;
+      // Create a marker per location, and put into markers array.
+      var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
+      // Push the marker to our array of markers.
+      markers.push(marker);
+      // Create an onclick event to open the large infowindow at each marker.
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+      });
+      // Two event listeners - one for mouseover, one for mouseout,
+      // to change the colors back and forth.
+      // marker.addListener('mouseover', function() {
+      //   this.setIcon(highlightedIcon);
+      // });
+      // marker.addListener('mouseout', function() {
+      //   this.setIcon(defaultIcon);
+      // });
+    }
+    document.getElementById('show-places').addEventListener('click', showPlaces);
+    document.getElementById('hide-places').addEventListener('click', hidePlaces);
+  
+  // This function populates the infowindow when the marker is clicked. We'll only allow
+  // one infowindow which will open at the marker that is clicked, and populate based
+  // on that markers position.
+  function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('<div>' + marker.title + '</div>');
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick', function() {
+        infowindow.marker = null;
+      });
+    }
+  }
+  // This function will loop through the markers array and display them all.
+  function showPlaces() {
+    var bounds = new google.maps.LatLngBounds();
+    // Extend the boundaries of the map for each marker and display the marker
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+      bounds.extend(markers[i].position);
     }
     map.fitBounds(bounds);
-}
-        // This function populates the infowindow when the marker is clicked. We'll only allow
-      // one infowindow which will open at the marker that is clicked, and populate based
-      // on that markers position.
-      function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-          });
+  }
+  // This function will loop through the listings and hide them all.
+  function hidePlaces() {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
     }
-      }
+  }}
